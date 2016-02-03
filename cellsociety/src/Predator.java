308efbbbced06c20812bc770e7cbec632677c9ae
Colette Torres <colette.torres@duke.edu>
2016-02-03 13:40:19 -0500

    starting predator class

---
+++ cellsociety/src/Predator.java
@@ -0,0 +1,20 @@
+
+public class Predator extends Simulation {
+
+	public Predator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateColors() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
