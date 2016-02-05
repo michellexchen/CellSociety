ab21527f02a996fbcd45c2142b03663179c4bf8e
michellexchen <michellechen617@gmail.com>
2016-02-04 21:29:58 -0500

    fixed fire class

--- cellsociety/src/Fire.java
+++ cellsociety/src/Fire.java
@@ -1,15 +1,17 @@
+import java.util.ArrayList;
+
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 
 public class Fire extends Simulation {
-	private double probCatch = .2;
+	private final static double probCatch = .2;
 	private int gridSize;
 	private Scene myScene;
 	private GridCell[][] myGrid;
-	//getcardinalneighbors
 	
 	public Fire() {
 		// TODO Auto-generated constructor stub
-		gridSize = getGridSize();
+		gridSize = 10;
 		myGrid = getCells();
 	}
 	
@@ -18,21 +20,67 @@ public class Fire extends Simulation {
 		//fill
 		for (int a = 0; a<gridSize; a++) { //row
 			for (int b = 0; b<gridSize; b++) {
-				//if (a & b ==0)
+				if (a == (gridSize-1)/2&& b == (gridSize-1)/2) {
+					myGrid[a][b] = new GridCell("BURNING", Color.RED);
+				}
+				if (a == 0 || b == 0 || a == gridSize-1 || b == gridSize-1){
+					myGrid[a][b] = new GridCell("EMPTY", Color.YELLOW);
+				}
+				myGrid[a][b] = new GridCell("TREE", Color.GREEN);
 			}
 		}
+		
+		initGridCells();		
+		return myScene;
 	}
-	
-	//function to set color
-	
+		
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
+		//go through entire grid 
+			//if empty: next state = empty
+			//if burning: next state = burning
+			//if tree: if neighbor burning: if random number between 0 and 1 < probCatch
+						//return burning, else tree
+					//if neighbor tree: next state = tree
+		for (int x = 0; x<gridSize; x++) { //assigning next state
+			for (int y = 0; y<gridSize; y++) {
+				GridCell curr = myGrid[x][y];
+				String currState = curr.getState();
+				if (currState == "EMPTY"){
+					curr.setNextState(currState);
+				}
+				if (currState == "BURNING") {
+					curr.setNextState("EMPTY");
+				}
+				if (currState == "TREE") {
+					ArrayList<GridCell> neighbors = getCardinalNeighbors(x,y);
+					if (!neighbors.contains("BURNING")) {
+						curr.setNextState(currState);
+					}
+					if (Math.random() < probCatch) {
+						curr.setNextState("BURNING");
+					}
+					else {curr.setNextState(currState);}
+				}
+			}
+		}		
+		updateStates();		
 	}
 
 	@Override
 	public void updateColors() {
-		// TODO Auto-generated method stub
+		for(int c = 0; c<gridSize; c++) {
+			for(int d = 0; d<gridSize; d++) {
+				GridCell current = myGrid[c][d]; 
+				if (current.getState().equals("EMPTY")) { //is it getnextState??? or getState?? confused bc of update call
+					current.setMyColor(Color.YELLOW);
+				}
+				else if (current.getState().equals("TREE")){
+					current.setMyColor(Color.GREEN);
+				}
+				current.setMyColor(Color.RED);
+			}
+		}
 		
 	}
 
