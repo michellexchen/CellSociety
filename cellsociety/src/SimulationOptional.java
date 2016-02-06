0e150f60aff76972a1bf8af17514428320f31570
michellexchen <michellechen617@gmail.com>
2016-02-06 17:29:33 -0500

    added check against faulty user input

---
+++ cellsociety/src/SimulationOptional.java
@@ -0,0 +1,23 @@
+
+public class SimulationOptional {
+	private Simulation simulation;
+	private Exception exception;
+	public SimulationOptional(Simulation sim, Exception except) {
+		simulation = sim;
+		exception = except;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean hasException() {
+		return exception != null;
+	}
+	
+	public String getExceptionMessage() {
+		return exception.getMessage();
+	}
+	
+	public Simulation getSimulation() {
+		return simulation;
+	}
+
+}
