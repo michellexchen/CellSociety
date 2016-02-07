21141148e9f2e66da159a1fe128b4cce5c90f7d8
michellexchen <michellechen617@gmail.com>
2016-02-07 00:10:09 -0500

    added javadoc comments for SimulationOptional class

--- cellsociety/src/SimulationOptional.java
+++ cellsociety/src/SimulationOptional.java
@@ -39,7 +39,8 @@ public class SimulationOptional {
 	/**
 	 * 
 	 * @return
-	 * Returns simulation, if XML file is corrupt, simulation is set to null in the XML reader class
+	 * Returns simulation
+	 * If XML file is corrupt, simulation is set to null in the XML reader class
 	 */
 	public Simulation getSimulation() {
 		return simulation;
